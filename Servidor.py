fb05dd5032bc03a6cdcd99682449e42ee7421077
JuhDeFreita <julia.carvalho@gec.inatel.br>
2025-04-04 21:42:34 -0300

    Correções no código

--- Servidor.py
+++ Servidor.py
@@ -6,12 +6,12 @@ import json
 def servidor():
     # Instanciando o gerador de dados
     dados_horario = Horario()
-    json_response = dados_horario.to_json_string    #JSON a ser enviado via servidor
+    json_response = dados_horario.to_json_string()    #JSON a ser enviado via servidor
 
     # Iniciando o servidor
     servidor = ModuloServidor(host='127.0.0.1', port=65432)
     servidor.socket_init()
-    servidor.envia_pacote(json_response)
+    servidor.enviar_dado(json_response)
 
 if __name__ == "__main__":
     servidor()
